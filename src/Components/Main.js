e5821fec226959e2c96be947ed9009d0ddd263bd
Akash375 <50778059+Akash375@users.noreply.github.com>
2021-04-28 02:10:50 +0530

    Updated Home Page & Created App details

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,67 +1,48 @@
 import React,{Component} from 'react';
-import { Navbar } from 'reactstrap';
-import {ReactComponent as homimg} from './asset1.svg';
-import Card from './CardComponent';
-import InfiniteScroll from 'react-infinite-scroll-component';
+//import {ReactComponent as homimg} from './asset1.svg';
+import RenderDetail from './DetailComponent';
+import LeftPanel from './LeftHomeComponent';
+import RightPanel from './RightHomeComponent';
 
 class Main extends Component{
     constructor(props){
         super(props);
 
         this.state={
-            items: Array.from({ length: 5 }),
-            hasMore: true
-        }
-
+            list: [],
+            show: false,
+            detailId: 0,
+            dQApp:"",
+            dQPublisher:""
+        };
+        this.back=this.back.bind(this);
+        this.detail=this.detail.bind(this);
     }
 
-    fetchMoreData = () => {
-        if (this.state.items.length >= 500) {
-          this.setState({ hasMore: false });
-          return;
-        }
-        setTimeout(() => {
-          this.setState({
-            items: this.state.items.concat(Array.from({ length: 5 }))
-          });
-        }, 500);
-      };
+    back(){
+        this.setState({show: false})
+    }
+
+    detail(appName,publisher,id){
+        this.setState({
+            dQApp: appName,
+            dQPublisher: publisher,
+            detailId: id,
+            show: true
+        });
+    }
 
     render(){
         return(
             <>
-                <div className="col-md-6" style={{float:"left",color:"#FFFFFF",padding:"0"}}>
-                    <div style={{background:"#7164FF", height:"50vh"}}>
-                        <Navbar>ADSOUL</Navbar>
-                        < homimg />
-                    </div>
-                    <div style={{background:"#6155DB", height:"50vh"}}>
-                        <h3 align="left">Revenue Optimization</h3>
-                    </div>
-                </div>
-                <div className="col-md-6" style={{float:"left",padding:"0"}}>
-                    <div className="jumbotron" style={{background:"#FFFFFF"}}>
-                        <h3 align="left">Apps</h3>
-                    </div>
-                    <div style={{background:"#F7F7F7"}}>
-                        <InfiniteScroll
-                        dataLength={this.state.items.length}
-                        next={this.fetchMoreData}
-                        hasMore={this.state.hasMore}
-                        loader={<h4>Loading...</h4>}
-                        height={550}
-                        endMessage={
-                            <p style={{ textAlign: "center" }}>
-                            <b>Yay! You have seen it all</b>
-                            </p>
-                        }
-                        >
-                        {this.state.items.map((i, index) => (
-                            <Card key={index} />
-                        ))}
-                        </InfiniteScroll>
-                    </div>
-                </div>
+            { !this.state.show?
+                <>
+                    <LeftPanel/>
+                    <RightPanel detail={this.detail}/>
+                </>
+                :
+                <RenderDetail id={this.state.detailId} back={this.back} appName={this.state.dQApp} publisher={this.state.dQPublisher}/>
+            }
             </>
                 
         );
